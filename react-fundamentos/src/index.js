2f9bca1265d4b2b4ae907bf868439cec1b4e2469
ATCR <atcr@gft.com>
2021-01-29 10:20:51 +0100

    mostrando valores en el dom, usando funciones

--- react-fundamentos/src/index.js
+++ react-fundamentos/src/index.js
@@ -2,5 +2,27 @@ import React from 'react'
 import ReactDom from 'react-dom'
 //alert("hello react");
 
-const App = <h1>Hola React</h1>
+
+
+const name = 'Jon Doe'
+const user = {
+  name:'Jon Doe',
+  age:13,
+  country:'Andorra'
+}
+
+const user1 = {
+  name:'Jon Doe',
+  age:26,
+  country:'Euskadi'
+}
+function getUserInfo(user){
+  return  `hola mi nombre es ${user.name} y soy de ${user.country}`
+}
+//getUserInfo=(user)=> `hola mi nombre es ${user.name} y soy de ${user.country}`
+
+//const App = <h1>Hola {name}</h1>
+const App = <h1>Tengo {user.age} años, el doble sería {user.age * 2}, {getUserInfo(user1)}</h1>
+
+
 ReactDom.render(App,document.getElementById('root'))
